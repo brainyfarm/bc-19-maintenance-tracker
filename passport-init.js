9b375265112333eb3e0b4bfca7446d6769917205
azemoh <joshazemoh@gmail.com>
2016-08-24 20:40:31 +0100

    build user signup and login

---
+++ passport-init.js
@@ -0,0 +1,78 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models').User;
+
+
+module.exports = function (passport) {
+
+  // Serialize Session
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+
+  // Deserialize Session
+
+  passport.deserializeUser((user, done) => {
+    User.find({ id: user.id }).then((user) => {
+      if (user) {
+        done(null, user);
+      } else {
+        done(new Error(), null);
+      }
+    });
+  });
+
+  // Auth
+  passport.use('login', new LocalStrategy({
+      passReqToCallback: true,
+      usernameField: 'email',
+      passwordField: 'password',
+    }, (req, email, password, done) => {
+        User.find({ email: email }).then((user) => {
+
+          if (user) { // Found user
+
+            if (User.validPassword(user, password)) { // Password match
+              return done(null, user);
+            } else {
+              console.log('Invalid Password');
+              return done(null, false); // redirect back to login page
+            }
+
+          } else {
+            console.log(`User Not Found with email ${username}`);
+            return done(null, false);
+          }
+        })
+
+      }));
+
+  passport.use('signup', new LocalStrategy({
+      passReqToCallback: true,
+      usernameField: 'email',
+      passwordField: 'password',
+    }, (req, email, password, done) => {
+
+      User.find({ email: email }).then((user) => {
+        if (user) {
+          console.log(`User already exists with email: ${username}`);
+          return done(null, false);
+        } else {
+          User.create({
+            name: req.body.name,
+            email: email,
+            password: password
+          }).then((user) => {
+            if (user) {
+              console.log(`${user.name} Registration succesful`);
+              return done(null, user);
+            } else {
+              console.log(`Failed to register ${user.name}`);
+              return done(null, false);
+            }
+          })
+        }
+      });
+
+    }));
+};
